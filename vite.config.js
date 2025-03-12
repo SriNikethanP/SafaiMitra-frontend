4ba9d5e0e02581cebaa042c1c081a12961a1ac6a
SriNikethanP <srinikethan.ponna@gmail.com>
2025-03-12 12:37:10 +0530

    update in ports

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // Allow external access
     port: process.env.PORT || 5173, // Use Render's assigned port
+    strictPort: true, // Prevent Vite from using a random port
+    allowedHosts: ["safaimitra.onrender.com"], // Allow Render domain
   },
 });
