d6ad12bf546c73546ed2a7ff2ca76c1830772b63
SriNikethanP <srinikethan.ponna@gmail.com>
2025-03-12 12:33:12 +0530

    vite update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Allow external access
+    port: process.env.PORT || 5173, // Use Render's assigned port
+  },
 });
